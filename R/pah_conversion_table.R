eb1e973a88be9148dfc04290d61ea6c22d0df236
Lisa Bramer <Lisa.Bramer@pnnl.gov>
2019-04-16 18:20:40 -0700

    Edit description file. Add PAH conversion data

---
+++ R/pah_conversion_table.R
@@ -0,0 +1,8 @@
+#' PAH Conversion Table
+#'
+#' Gives molecular weight (grams/mol) of each Chemical
+#'
+#' @format A 63x4 data.frame giving FSES parameter id, chemical name, CAS code, and molecular weight
+#' @name pah_conversion_table
+#' @rdname pah_conversion_table
+NULL
